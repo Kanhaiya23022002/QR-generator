8c2a49ea3a578b1441a403c9152b3cd01cf1c9de
Kanhaiya23022002 <kanhaiyajha270@gmail.com>
2024-03-10 21:16:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var container=document.querySelector(".container");
+var generateBtn=document.querySelector(".generate-btn");
+var qrInput=document.querySelector(".qr-input");
+var qrImg=document.querySelector(".qr-image");
+
+generateBtn.onclick=function(){
+    if(qrInput.value.length > 0){
+        generateBtn.innerText = "Generating QR code.........."
+        qrImg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrInput.value;
+        qrImg.onload = function (){
+            container.classList.add("active");
+        }
+    }
+}
